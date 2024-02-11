0a359c44a9073ce3f80c3e89854bec3dfe717cc7
MtchedlishviliLevani <122740187+MtchedlishviliLevani@users.noreply.github.com>
2024-02-11 15:10:04 +0400

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -219,7 +219,7 @@ export const cartPageContent: cartPageContentType = {
         // fix
         "./assets/images/product-xx99-mark-two-headphones/mobile/image-product.jpg",
       tablet:"../../src/assets/images/product-xx99-mark-two-headphones/tablet/image-product.jpg",
-      desktop: "../../src/assets/images/product-xx99-mark-two-headphones/desktop/image-product.jpg"),
+      desktop: "../../src/assets/images/product-xx99-mark-two-headphones/desktop/image-product.jpg",
     },
     price: 2999,
     paragraph:
